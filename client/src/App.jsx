f78937187f4132be1da251aafbaed65284019910
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-05-17 00:03:24 +0530

    feat(client): complete nav bar and style as well

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,12 @@ import { Nav } from './components'
 
 const App = () => {
 	return (
-		<>
+		<div className="container mx-auto">
 			<Nav />
 			<Routes>
 				<Route path="/" element={<Home />} />
 			</Routes>
-		</>
+		</div>
 	)
 };
 
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+import LogoMark from '../assets/logo-mark.svg'
+import { Link } from 'react-router-dom'
+
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className='flex justify-between items-center py-5'>
+      <Link to="/" className="flex gap-4 text-lg md:text-xl font-semibold items-center">
+        <img src={LogoMark} alt="Open Pen Logo" className='w-4 md:w-6' />
+        <span className=''>/</span>
+        <span>Open Pen</span>
+      </Link>
+      <div className="flex gap-4 items-center">
+        <Link to="/signin" className='px-7 py-3 font-medium border-black border-2 hover:bg-black hover:text-white'>Sign in</Link>
+        <Link to="/signup" className='px-7 py-3 font-medium bg-black text-white border-black border-2 hover:bg-white hover:text-black'>Sign up</Link>
+      </div>
+    </nav>
   )
 }
 
