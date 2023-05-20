797ded28fc81a5fba6f2e2eb6cbb4ee7fecf5cf9
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-05-20 12:35:15 +0530

    feat(server): add signup route with creating user in database
    installed mongoose and bcrypt
    set up route for signup
    created User schema and model
    utilized it to add new users in databse
    hashed passwords with bcrypt

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 dotenv.config();
 
@@ -16,6 +18,40 @@ if (process.env.NODE_ENV === "development") {
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
+
+const UserSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		min: 3,
+		max: 20,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	}
+});
+
+const UserModel = mongoose.model("User", UserSchema);
+
+app.post("/signup", async (req, res) => {
+	const { username, password } = req.body;
+	try {
+		const hashedPassword = await bcrypt.hash(password, 10);
+		const user = { username, password: hashedPassword };
+		const newUser = await UserModel.create(user)
+		res.status(201).json(newUser);
+	} catch (err) {
+		// console.log(err);
+		res.status(500).json(err);
+	}
+});
+
+mongoose.connect(process.env.DATABASE_URI);
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
