010269cd97b74ba09d6ed839da12c394d5ca4d34
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-05-20 11:49:41 +0530

    feat(client): add basic form handling
    added basic form handling with useState hook in Signin page and signup page
    added props to Input component

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 import { Input, ButtonFull } from "../components";
 
 const Signin = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
 	return (
 		<div className="mt-12 w-96 mx-auto">
 			<h1 className="font-display text-4xl font-bold text-center">
 				Welcome Back
 			</h1>
 			<form action="" className="mt-6">
-        <Input type="text" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
+        <Input type="text" placeholder="Email" value={email} setValue={setEmail} />
+        <Input type="password" placeholder="Password" value={password} setValue={setPassword}/>
         <ButtonFull text="Sign In" type="submit" />
 			</form>
       <p className="text-center mt-3 text-sm md:text-base text-gray-500">Don't have an account? <Link to="/signup" className="text-black underline text-medium">Sign Up</Link></p>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 import { Input, ButtonFull } from "../components";
 
 const Signup = () => {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
 	return (
 		<div className="mt-12 w-96 mx-auto">
 			<h1 className="font-display text-4xl font-bold text-center">
 				Create your account
 			</h1>
 			<form action="" className="mt-6">
-				<Input type="text" placeholder="Name" />
-				<Input type="text" placeholder="Email" />
-				<Input type="password" placeholder="Password" />
+				<Input type="text" placeholder="Name" value={name} setValue={setName} />
+				<Input type="text" placeholder="Email" value={email} setValue={setEmail} />
+				<Input type="password" placeholder="Password" value={password} setValue={setPassword} />
 				<ButtonFull text="Create Account" type="submit" />
 			</form>
 			<p className="text-center mt-3 text-sm md:text-base text-gray-500">
