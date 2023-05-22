79dbda72a96f97fb9fb10c1dbf624d0e9bd8ca6e
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-05-23 00:56:34 +0530

    fix(server): set up cors for cookies

--- server/app.js
+++ server/app.js
@@ -14,7 +14,10 @@ const app = express();
 
 // Setting up the middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+	origin: "http://localhost:5173",
+	credentials: true,
+}));
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
