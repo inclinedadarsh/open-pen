1792d79a44bc2d7c2f16421358afae1f03af5f01
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-06-08 17:57:32 +0530

    feat(signup): :sparkles: add functionality to login user as soon as they create an account and then redirect to home page

--- server/routes/signupRoute.js
+++ server/routes/signupRoute.js
@@ -5,8 +5,10 @@ const router = express.Router();
 // Importing Handlers
 // import { signupHandler } from "../handlers";
 import signupHandler from "../handlers/signupHandler.js";
+import loginHandler from "../handlers/loginHandler.js";
 
 // Setting up the routes
-router.post("/", signupHandler);
+router.use(signupHandler)
+router.post("/", loginHandler);
 
 export default router;
